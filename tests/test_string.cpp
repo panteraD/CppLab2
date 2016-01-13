7a6d114235d38f184279e7e6af28f73216a1aa25
doller <chernenkov.andrei@Yandex.ru>
2016-01-13 23:42:50 +0300

    test travis

--- tests/test_string.cpp
+++ tests/test_string.cpp
@@ -1,24 +1,24 @@
-//#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-//
-//#include "catch.hpp"
-//#include "String.h"
-//
-////unsigned int Factorial( unsigned int number ) {
-////    return number <= 1 ? number : Factorial(number-1)*number;
-////}
-//
-////
-////lets start test
-//void cmp_string(const String &lhs, const String &rhs) {
-//    assert(lhs.size()==rhs.size());
-//    for (std::size_t i = 0; i < lhs.size(); ++i) {
-//        REQUIRE(lhs[i] == rhs[i]);
-//    }
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include "String.h"
+
+//unsigned int Factorial( unsigned int number ) {
+//    return number <= 1 ? number : Factorial(number-1)*number;
 //}
+
 //
-//TEST_CASE( "FUCK YOu" ) {
-//    String s;
-//    char  expected[] = "";
-//    cmp_string(s, expected);
-//
-//}
+//lets start test
+void cmp_string(const String &lhs, const String &rhs) {
+    assert(lhs.size()==rhs.size());
+    for (std::size_t i = 0; i < lhs.size(); ++i) {
+        REQUIRE(lhs[i] == rhs[i]);
+    }
+}
+
+TEST_CASE( "FUCK YOu" ) {
+    String s;
+    char  expected[] = "";
+    cmp_string(s, expected);
+
+}
