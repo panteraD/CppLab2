3004d9328eb9db2b808ef17df193211d077e4ad8
doller <chernenkov.andrei@Yandex.ru>
2016-01-13 19:19:37 +0300

    Simple test

---
+++ tests/test_string.cpp
@@ -0,0 +1,23 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "String.h"
+
+//unsigned int Factorial( unsigned int number ) {
+//    return number <= 1 ? number : Factorial(number-1)*number;
+//}
+
+
+//lets start test
+void cmp_string(const String &lhs, const String &rhs) {
+    assert(lhs.size()==rhs.size());
+    for (size_t i = 0; i < lhs.size(); ++i) {
+        REQUIRE(lhs[i] == rhs[i]);
+    }
+}
+
+TEST_CASE( "FUCK YOu" ) {
+    String s;
+    char  expected[] = "";
+    cmp_string(s, expected);
+
+}
